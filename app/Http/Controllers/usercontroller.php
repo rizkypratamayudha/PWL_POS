29e8eb45dff49c318d43d2d37d6c6e34f5ef16a6
rizkypratamayudha <masrizki87@gmail.com>
2024-09-25 17:23:15 +0700

    JS 5 Praktikum 4

--- app/Http/Controllers/usercontroller.php
+++ app/Http/Controllers/usercontroller.php
@@ -24,13 +24,18 @@ class usercontroller extends Controller
         ];
 
         $activeMenu = 'user'; //set menu yang aktif
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = levelmodel::all(); //mengambil data level untuk filter level
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'level'=>$level]);
     }
 
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        if ($request->level_id){
+            $users->where('level_id',$request->level_id);
+        }
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addIndexColumn()
