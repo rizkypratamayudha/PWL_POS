9025d5cda897a7ec345b0ef080477e31cbb394e9
rizkypratamayudha <masrizki87@gmail.com>
2024-09-25 09:37:31 +0700

    JS 5 Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\dashboardController;
 use App\Http\Controllers\kategoricontroller;
 use App\Http\Controllers\levelcontroller;
 use App\Http\Controllers\usercontroller;
@@ -16,21 +17,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/level',[levelcontroller::class, 'index']);
-Route::get('/kategori',[kategoricontroller::class,'index']);
+// Route::get('/level',[levelcontroller::class, 'index']);
+// Route::get('/kategori',[kategoricontroller::class,'index']);
 
-Route::get('/user',[usercontroller::class,'index']);
+// Route::get('/user',[usercontroller::class,'index']);
 
-Route::get('/user/tambah',[usercontroller::class, 'tambah']);
-Route::post('/user/tambah_simpan',[usercontroller::class,'tambah_simpan']);
+// Route::get('/user/tambah',[usercontroller::class, 'tambah']);
+// Route::post('/user/tambah_simpan',[usercontroller::class,'tambah_simpan']);
 
-Route::get('/user/ubah/{id}',[usercontroller::class,'ubah']);
-Route::put('/user/ubah_simpan/{id}',[usercontroller::class,'ubah_simpan']);
+// Route::get('/user/ubah/{id}',[usercontroller::class,'ubah']);
+// Route::put('/user/ubah_simpan/{id}',[usercontroller::class,'ubah_simpan']);
 
-Route::get('/user/hapus/{id}',[usercontroller::class,'hapus']);
+// Route::get('/user/hapus/{id}',[usercontroller::class,'hapus']);
 
-Route::get('/indexrelationship',[usercontroller::class,'indexrelationship']);
+// Route::get('/indexrelationship',[usercontroller::class,'indexrelationship']);
+
+Route::get('/dashboard',[dashboardController::class,'index']);
