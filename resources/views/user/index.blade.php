deee760e1fab6d9585c1d99c9f82fba60c12769a
rizkypratamayudha <masrizki87@gmail.com>
2024-10-02 10:27:13 +0700

    JS 6 Praktikum 3

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.template')
+
 @section('content')
     <div class="card card-outline card-primary">
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
-                <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
-                    ajax</button>
+                <button onclick="modalAction('{{ url('/user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                    Ajax</button>
             </div>
         </div>
         <div class="card-body">
-            @if (@session('success'))
+            @if (session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
             @endif
+
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
             <div class="row">
                 <div class="col-md-12">
                     <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter </label>
+                        <label class="col-1 control-label col-form-label">Filter:</label>
                         <div class="col-3">
                             <select class="form-control" id="level_id" name="level_id" required>
                                 <option value="">- Semua -</option>
@@ -32,6 +34,7 @@
                     </div>
                 </div>
             </div>
+
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
@@ -45,11 +48,13 @@
             </table>
         </div>
     </div>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" databackdrop="static"
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
         data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
+
 @push('css')
 @endpush
+
 @push('js')
     <script>
         function modalAction(url = '') {
@@ -57,11 +62,10 @@
                 $('#myModal').modal('show');
             });
         }
-
-        var dataUser;
+        var datauser;
         $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
-                // serverSide: true, jika ingin menggunakan server side processing
+            dataUser = $('#table_user').DataTable({
+                // serverSide: true, jika ingin menggunakan server side proses 
                 serverSide: true,
                 ajax: {
                     "url": "{{ url('user/list') }}",
@@ -72,7 +76,7 @@
                     }
                 },
                 columns: [{
-                    // nomor urut dari laravel datatable addIndexColumn()
+                    // nomor urut dari laravel datatable addIndexColumn() 
                     data: "DT_RowIndex",
                     className: "text-center",
                     orderable: false,
@@ -80,9 +84,9 @@
                 }, {
                     data: "username",
                     className: "",
-                    // orderable: true, jika ingin kolom ini bisa diurutkan
+                    // orderable: true, jika ingin kolom ini bisa diurutkan  
                     orderable: true,
-                    // searchable: true, jika ingin kolom ini bisa dicari
+                    // searchable: true, jika ingin kolom ini bisa dicari 
                     searchable: true
                 }, {
                     data: "nama",
@@ -90,7 +94,7 @@
                     orderable: true,
                     searchable: true
                 }, {
-                    // mengambil data level hasil dari ORM berelasi
+                    // mengambil data level hasil dari ORM berelasi 
                     data: "level.level_nama",
                     className: "",
                     orderable: false,
@@ -104,7 +108,7 @@
             });
             $('#level_id').on('change', function() {
                 dataUser.ajax.reload();
-            })
+            });
         });
     </script>
 @endpush
