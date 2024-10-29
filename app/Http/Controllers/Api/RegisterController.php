51a63ec135b935cb1c5b028afab7412c1319f69f
rizkypratamayudha <masrizki87@gmail.com>
2024-10-29 20:48:51 +0700

    JS 10 Praktikum 2

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    public function register(Request $request){
+    public function __invoke(Request $request){
         $validator = Validator::make($request->all(),[
             'username'=>'required',
             'nama'=>'required',
